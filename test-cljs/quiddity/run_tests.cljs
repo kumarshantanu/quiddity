5fa7b8d2eb933a059ceca7634d52450a7b085e14
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-09-07 13:46:23 +0530

    add CLJS test for creating anonymous functions

--- test-cljs/quiddity/run_tests.cljs
+++ test-cljs/quiddity/run_tests.cljs
@@ -1,6 +1,7 @@
 (ns quiddity.run-tests
   (:require [quiddity.core-test      :as core-test]
             [quiddity.lib-test       :as lib-test]
+            [quiddity.lib-cljs-test  :as lib-cljs-test]
             [clip-test.testutil-cljs :as tu]))
 
 
@@ -8,4 +9,5 @@
   []
   (core-test/test-ns-hook)
   (lib-test/test-ns-hook)
+  (lib-cljs-test/test-ns-hook)
   (tu/print-test-summary))
